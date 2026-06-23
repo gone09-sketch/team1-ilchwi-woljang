eb0ddcadc78faf9b0c1e405b6a0b3ed6ffb0891a
송이 <springday3355@gmail.com>
2026-06-23 17:23:04 +0900

    feat: 상품담기 API 구현

--- src/main/java/com/team1ilchwiwoljang/domain/cart/controller/CartController.java
+++ src/main/java/com/team1ilchwiwoljang/domain/cart/controller/CartController.java
@@ -1,7 +1,36 @@
 package com.team1ilchwiwoljang.domain.cart.controller;
 
+import com.team1ilchwiwoljang.common.response.ApiResponse;
+import com.team1ilchwiwoljang.domain.cart.dto.CartCreateRequest;
+import com.team1ilchwiwoljang.domain.cart.dto.CartResponse;
+import com.team1ilchwiwoljang.domain.cart.repository.CartRepository;
+import com.team1ilchwiwoljang.domain.cart.service.CartService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/carts")
 public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("/items")
+    public ResponseEntity<ApiResponse<CartResponse>> addCartItem(
+            @AuthenticationPrincipal Long memberId,
+            @Valid @RequestBody CartCreateRequest request
+            ){
+        CartResponse response = cartService.addCartItem(memberId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.of(response));
+
+    }
+
+
 }
