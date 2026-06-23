e7e5603dbfe05c12374478e8ecded5bcc3474a68
minbyeongjun <byeongjunmin00@gmail.com>
2026-06-23 12:49:32 +0900

    feat(Order, OrderItem 엔티티 매핑 추가

--- src/main/java/com/team1ilchwiwoljang/domain/order/entity/Order.java
+++ src/main/java/com/team1ilchwiwoljang/domain/order/entity/Order.java
@@ -1,7 +1,48 @@
 package com.team1ilchwiwoljang.domain.order.entity;
 
-import jakarta.persistence.Entity;
+import com.team1ilchwiwoljang.common.entity.BaseEntity;
+import com.team1ilchwiwoljang.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Order {
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String orderNumber;
+
+    private Long totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private OrderStatus orderStatus;
+
+    private Long pgAmount;
+
+    private LocalDateTime paidAt;
+
+    private LocalDateTime canceledAt;
+
+    public static Order create(Member member, String orderNumber, Long totalAmount, Long pgAmount) {
+        Order order = new Order();
+        order.member = member;
+        order.orderNumber = orderNumber;
+        order.totalAmount = totalAmount;
+        order.pgAmount = pgAmount;
+        order.orderStatus = OrderStatus.PENDING;
+        return order;
+    }
 }
