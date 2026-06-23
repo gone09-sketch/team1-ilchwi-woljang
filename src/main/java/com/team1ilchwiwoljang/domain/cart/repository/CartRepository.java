1d7f5d0dca108c7df3045d3d1fc26fd9cb565744
송이 <springday3355@gmail.com>
2026-06-23 17:08:34 +0900

    feat: 장바구니 서비스 구현

--- src/main/java/com/team1ilchwiwoljang/domain/cart/repository/CartRepository.java
+++ src/main/java/com/team1ilchwiwoljang/domain/cart/repository/CartRepository.java
@@ -1,7 +1,16 @@
 package com.team1ilchwiwoljang.domain.cart.repository;
 
+import com.team1ilchwiwoljang.domain.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+
 @Repository
-public interface CartRepository {
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByMemberIdAndProductId(Long memberId, Long productId);
+
+
 }
