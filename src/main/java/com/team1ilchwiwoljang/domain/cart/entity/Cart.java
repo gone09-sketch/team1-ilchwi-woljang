03cc18364b3b4af61f0cf7c69dce9ba03dcfdc7a
han-yejin <biziyejin8844@naver.com>
2026-06-23 11:56:46 +0900

    feat: 장바구니 상품 엔티티 매핑 추가

--- src/main/java/com/team1ilchwiwoljang/domain/cart/entity/Cart.java
+++ src/main/java/com/team1ilchwiwoljang/domain/cart/entity/Cart.java
@@ -1,7 +1,52 @@
 package com.team1ilchwiwoljang.domain.cart.entity;
 
-import jakarta.persistence.Entity;
+import com.team1ilchwiwoljang.domain.member.entity.Member;
+import com.team1ilchwiwoljang.domain.product.entity.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(
+        name = "carts",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_carts_member_product",
+                        columnNames = {"member_id", "product_id"}
+                )
+        }
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = true)
+    private Product product;
+
+    @Column(nullable = false, columnDefinition = "int UNSIGNED DEFAULT 1")
+    private int quantity;
+
+    private Cart(Member member, Product product, int quantity) {
+        this.member = member;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public static Cart create(Member member, Product product, int quantity) {
+        return new Cart(member, product, quantity);
+    }
+
+    public void increaseQuantity(int quantity) {
+        this.quantity += quantity;
+    }
 }
