b152a0b1c5baf2cc002e8b6201ba741968e4d302
송이 <springday3355@gmail.com>
2026-06-23 17:23:04 +0900

    feat: 상품담기 API 구현

--- src/main/java/com/team1ilchwiwoljang/domain/cart/controller/CartController.java
+++ src/main/java/com/team1ilchwiwoljang/domain/cart/controller/CartController.java
@@ -1,21 +1,34 @@
 package com.team1ilchwiwoljang.domain.cart.controller;
 
 import com.team1ilchwiwoljang.common.response.ApiResponse;
+import com.team1ilchwiwoljang.domain.cart.dto.CartAddResponse;
+import com.team1ilchwiwoljang.domain.cart.dto.CartCreateRequest;
 import com.team1ilchwiwoljang.domain.cart.dto.response.CartResponse;
 import com.team1ilchwiwoljang.domain.cart.service.CartService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/carts")
 @RequiredArgsConstructor
+@RequestMapping("/api/carts")
+
 public class CartController {
 
     private final CartService cartService;
+
+    @PostMapping("/items")
+    public ResponseEntity<ApiResponse<CartAddResponse>> addCartItem(
+            @AuthenticationPrincipal Long memberId,
+            @Valid @RequestBody CartCreateRequest request
+            ){
+        CartAddResponse response = cartService.addCartItem(memberId, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.of(response));
+
+    }
 
     @GetMapping
     public ResponseEntity<ApiResponse<CartResponse>> getCarts(@RequestParam Long memberId) {
