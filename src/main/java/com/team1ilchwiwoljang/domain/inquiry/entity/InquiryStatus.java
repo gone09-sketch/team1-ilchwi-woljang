9b5c3a64af20efbea761189b5e8b240abef73010
JeonYongWoon <oioipo22@gmail.com>
2026-06-23 19:45:00 +0900

    refactor: PR 리뷰 피드백 반영 - ERD 명세에 맞춰 answer 컬럼 매핑명 변경, answeredAt 추가, InquiryStatus에 CLOSED 추가

--- src/main/java/com/team1ilchwiwoljang/domain/inquiry/entity/InquiryStatus.java
+++ src/main/java/com/team1ilchwiwoljang/domain/inquiry/entity/InquiryStatus.java
@@ -2,5 +2,6 @@ package com.team1ilchwiwoljang.domain.inquiry.entity;
 
 public enum InquiryStatus {
     WAITING,   // 답변 대기
-    ANSWERED   // 답변 완료
+    ANSWERED,  // 답변 완료
+    CLOSED     // 문의 종료
 }
