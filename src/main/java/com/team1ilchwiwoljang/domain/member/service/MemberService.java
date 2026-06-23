c130c6894a2062a2474f8674f0d03765ba90cdc7
송이 <springday3355@gmail.com>
2026-06-23 17:08:55 +0900

    feat: Member 조회 서비스 임시 구현

--- src/main/java/com/team1ilchwiwoljang/domain/member/service/MemberService.java
+++ src/main/java/com/team1ilchwiwoljang/domain/member/service/MemberService.java
@@ -1,7 +1,22 @@
 package com.team1ilchwiwoljang.domain.member.service;
 
+import com.team1ilchwiwoljang.common.exception.BusinessException;
+import com.team1ilchwiwoljang.common.exception.ErrorCode;
+import com.team1ilchwiwoljang.domain.member.entity.Member;
+import com.team1ilchwiwoljang.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Transactional(readOnly = true)
+    public Member getMember(Long memberId){
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
+    }
 }
