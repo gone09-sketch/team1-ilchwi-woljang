bf89dd95f025f69c73bf5494f11f08fdda1e8d01
송이 <springday3355@gmail.com>
2026-06-23 17:09:08 +0900

    feat: Product 조회 서비스 임시 구현

--- src/main/java/com/team1ilchwiwoljang/domain/product/service/ProductService.java
+++ src/main/java/com/team1ilchwiwoljang/domain/product/service/ProductService.java
@@ -1,7 +1,23 @@
 package com.team1ilchwiwoljang.domain.product.service;
 
+import com.team1ilchwiwoljang.common.exception.BusinessException;
+import com.team1ilchwiwoljang.common.exception.ErrorCode;
+import com.team1ilchwiwoljang.domain.product.entity.Product;
+import com.team1ilchwiwoljang.domain.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
-}
+
+    private final ProductRepository productRepository;
+
+    @Transactional(readOnly = true)
+    public Product getProduct(Long productId){
+        return productRepository.findById(productId)
+            .orElseThrow(()-> new BusinessException(ErrorCode.PRODUCT_NOT_FOUND));
+        }
+    }
+
