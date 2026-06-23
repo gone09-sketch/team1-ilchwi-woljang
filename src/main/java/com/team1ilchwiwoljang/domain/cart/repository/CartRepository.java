a6c5385460adcbf444ef4b472ea7e4afe9ebff68
송이 <springday3355@gmail.com>
2026-06-23 17:08:34 +0900

    feat: 장바구니 서비스 구현

--- src/main/java/com/team1ilchwiwoljang/domain/cart/repository/CartRepository.java
+++ src/main/java/com/team1ilchwiwoljang/domain/cart/repository/CartRepository.java
@@ -2,6 +2,8 @@ package com.team1ilchwiwoljang.domain.cart.repository;
 
 import com.team1ilchwiwoljang.domain.cart.entity.Cart;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,8 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
             order by c.id desc
             """)
     List<Cart> findAllByMemberIdWithProduct(@Param("memberId") Long memberId);
+
+    Optional<Cart> findByMemberIdAndProductId(Long memberId, Long productId);
+
+
 }
