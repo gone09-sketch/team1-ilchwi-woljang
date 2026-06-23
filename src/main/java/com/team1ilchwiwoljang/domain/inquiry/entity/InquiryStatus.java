93784ce4b1e26fcd7daa605e4ce03712a4566d79
JeonYongWoon <oioipo22@gmail.com>
2026-06-23 16:41:52 +0900

    feat: 고객 문의 생성 API 구현 (POST /api/members/inquiry)

---
+++ src/main/java/com/team1ilchwiwoljang/domain/inquiry/entity/InquiryStatus.java
@@ -0,0 +1,6 @@
+package com.team1ilchwiwoljang.domain.inquiry.entity;
+
+public enum InquiryStatus {
+    WAITING,   // 답변 대기
+    ANSWERED   // 답변 완료
+}
--- src/main/java/com/team1ilchwiwoljang/domain/order/entity/Order.java
+++ src/main/java/com/team1ilchwiwoljang/domain/order/entity/Order.java
@@ -1,7 +1,13 @@
 package com.team1ilchwiwoljang.domain.order.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
