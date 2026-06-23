88ef1581682a66d77787cc7b861e54abae07de43
송이 <springday3355@gmail.com>
2026-06-23 17:09:17 +0900

    feat: 회원/상품 조회 실패 에러코드 추가

--- src/main/java/com/team1ilchwiwoljang/common/exception/ErrorCode.java
+++ src/main/java/com/team1ilchwiwoljang/common/exception/ErrorCode.java
@@ -20,8 +20,13 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
     DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다."),
 
+    // product
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND, "상품을 찾을 수 없습니다."),
+
     // cart
     INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "수량은 1개 이상이어야 합니다.");
+
+
 
     private final HttpStatus status;
     private final String message;
