af68128209ac0aad4a3f3d34a52bae321c82e747
Jiwon Yang <g.one9709@gmail.com>
2026-06-25 01:55:21 +0900

    fix: 장바구니 상품 연관관계를 필수로 제한

--- src/main/java/com/team1ilchwiwoljang/domain/cart/entity/Cart.java
+++ src/main/java/com/team1ilchwiwoljang/domain/cart/entity/Cart.java
@@ -32,8 +32,8 @@ public class Cart extends BaseEntity {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
     @Column(nullable = false)
