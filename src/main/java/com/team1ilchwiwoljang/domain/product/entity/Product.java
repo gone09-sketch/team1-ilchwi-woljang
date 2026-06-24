1ead6265a90be71bb892658fe6d5195b395f9698
minbyeongjun <byeongjunmin00@gmail.com>
2026-06-24 15:51:42 +0900

    fix(order): 코드 리뷰 반영

--- src/main/java/com/team1ilchwiwoljang/domain/product/entity/Product.java
+++ src/main/java/com/team1ilchwiwoljang/domain/product/entity/Product.java
@@ -50,6 +50,9 @@ public class Product extends BaseEntity {
     }
 
     public void decreaseStock(int quantity) {
+        if (this.stock < quantity) {
+            throw new IllegalArgumentException("재고가 부족합니다.");
+        }
         this.stock -= quantity;
     }
 }
