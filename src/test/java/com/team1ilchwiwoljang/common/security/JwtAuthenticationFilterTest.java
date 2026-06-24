ae1f196a5a5b451c4d3508c46d968a89da5b6a90
Jiwon Yang <g.one9709@gmail.com>
2026-06-24 17:36:46 +0900

    test: JWT role 변경에 맞춰 인증 테스트 수정

--- src/test/java/com/team1ilchwiwoljang/common/security/JwtAuthenticationFilterTest.java
+++ src/test/java/com/team1ilchwiwoljang/common/security/JwtAuthenticationFilterTest.java
@@ -2,6 +2,7 @@ package com.team1ilchwiwoljang.common.security;
 
 import com.team1ilchwiwoljang.common.exception.ErrorCode;
 import com.team1ilchwiwoljang.common.security.auth.AuthMember;
+import com.team1ilchwiwoljang.domain.member.entity.MemberRole;
 import com.team1ilchwiwoljang.domain.member.service.MemberService;
 import jakarta.servlet.FilterChain;
 import org.junit.jupiter.api.AfterEach;
@@ -56,6 +57,7 @@ class JwtAuthenticationFilterTest {
         request.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN);
 
         given(jwtTokenProvider.getMemberId(ACCESS_TOKEN)).willReturn(MEMBER_ID);
+        given(jwtTokenProvider.getRole(ACCESS_TOKEN)).willReturn(MemberRole.MEMBER);
         given(memberService.existsActiveMember(MEMBER_ID)).willReturn(true);
 
         // when
@@ -65,7 +67,10 @@ class JwtAuthenticationFilterTest {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         assertThat(authentication).isNotNull();
         assertThat(authentication.getPrincipal())
-                .isEqualTo(new AuthMember(MEMBER_ID));
+                .isEqualTo(new AuthMember(MEMBER_ID, MemberRole.MEMBER));
+        assertThat(authentication.getAuthorities())
+                .extracting("authority")
+                .containsExactly("ROLE_MEMBER");
 
         verify(filterChain).doFilter(request, response);
         verify(securityErrorResponseHandler, never()).writeErrorResponse(any(), any());
@@ -81,6 +86,7 @@ class JwtAuthenticationFilterTest {
         request.addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + ACCESS_TOKEN);
 
         given(jwtTokenProvider.getMemberId(ACCESS_TOKEN)).willReturn(MEMBER_ID);
+        given(jwtTokenProvider.getRole(ACCESS_TOKEN)).willReturn(MemberRole.MEMBER);
         given(memberService.existsActiveMember(MEMBER_ID)).willReturn(false);
 
         // when
