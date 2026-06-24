f1ec2fdc96c3072c3b863398ca7b8378ba797705
송이 <springday3355@gmail.com>
2026-06-24 12:34:44 +0900

    fix:MemberService 빈줄 제거

--- src/main/java/com/team1ilchwiwoljang/domain/member/service/MemberService.java
+++ src/main/java/com/team1ilchwiwoljang/domain/member/service/MemberService.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-
 public class MemberService {
 
     private final MemberRepository memberRepository;
