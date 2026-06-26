1171045ed11bb22b25f6876b52192e69cb974d61
minbyeongjun <byeongjunmin00@gmail.com>
2026-06-26 16:32:23 +0900

    feat: 상품 목록 조회 복합 인덱스 추가

--- src/main/java/com/team1ilchwiwoljang/domain/product/entity/Product.java
+++ src/main/java/com/team1ilchwiwoljang/domain/product/entity/Product.java
@@ -2,23 +2,19 @@ package com.team1ilchwiwoljang.domain.product.entity;
 
 import com.team1ilchwiwoljang.common.entity.BaseEntity;
 import com.team1ilchwiwoljang.domain.category.entity.Category;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "products")
+@Table(
+        name = "products",
+        indexes = {
+                @Index(name = "idx_product_status_created_at", columnList = "status, created_at"),
+                @Index(name = "idx_product_category_status_created_at", columnList = "category_id, status, created_at")
+        }
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product extends BaseEntity {
