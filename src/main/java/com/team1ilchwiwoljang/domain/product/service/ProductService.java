f820fcc4e53e36aff62cda1c87265867582acfaa
송이 <springday3355@gmail.com>
2026-07-01 21:07:37 +0900

    refactor: 트랜잭셔널 수정

--- src/main/java/com/team1ilchwiwoljang/domain/product/service/ProductService.java
+++ src/main/java/com/team1ilchwiwoljang/domain/product/service/ProductService.java
@@ -37,7 +37,7 @@ public class ProductService {
     /**
      * 주문 재고 차감
      */
-    @Transactional(readOnly = true)
+    @Transactional
     public Product getProductWithPessimisticLock(Long productId){
         return productRepository.findByWithPessimisticLock(productId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.PRODUCT_NOT_FOUND));
